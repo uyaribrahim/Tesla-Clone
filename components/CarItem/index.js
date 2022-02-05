005e5f1687f7cc9acfc33b8368c9c61ad25a27e0
İbrahim <iuyar99@gmail.com>
2022-02-05 13:57:11 -0800

    create a separate component for Button

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {View, Text, ImageBackground} from 'react-native';
+import StyledButton from '../StyledButton';
 import styles from './styles';
 
 const CarItem = (props) => {
     return (
         <View style={styles.carContainer}>
+
         <ImageBackground
         source={require('../../assets/images/ModelX.jpeg')}
-        style={styles.image}/>
+        style={styles.image}
+        />
 
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subTitle}>Starting at $69,500</Text>
         </View>
+        
+       <StyledButton></StyledButton>
+
       </View>
     );
 };
---
+++ components/StyledButton/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+
+    container: {
+        width: '100%',
+        padding: 13,
+        
+    },
+    button: {
+        height: 40,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 13,
+        fontWeight: '500',
+        textTransform: 'uppercase'
+    }
+});
+
+
+export default styles;
