a27d2cf5a5080d5641790d55a0457a3a0ccab97b
İbrahim <iuyar99@gmail.com>
2022-02-06 03:30:13 -0800

    render a list of items: FlatList

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import CarItem from './components/CarItem';
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <CarItem
-        name = {"Model 3"}
-        tagline = {"Order Online For"}
-        taglineCTA = {"Touchless Delivery"}
-        image = {require('./assets/images/Model3.jpeg')}/>
+      <CarsList></CarsList>
 
-      <StatusBar style='auto'/>
+      <StatusBar style='inverted'/>
     </View>
   );
 }
---
+++ components/CarsList/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text, FlatList, Dimensions} from 'react-native'
+import CarItem from '../CarItem';
+
+import styles from './styles'
+import cars from './cars'
+
+
+const CarsList = (props) => {
+   return (
+       <View style = {styles.container}>
+           <FlatList
+               data={cars}
+               renderItem={({item}) => <CarItem car={item}/>}
+               snapToAlignment={'start'}
+               showsVerticalScrollIndicator={false}
+               decelerationRate={'fast'}
+               snapToInterval={Dimensions.get('screen').height}
+            />
+       </View>
+    );
+};
+
+
+export default CarsList;
